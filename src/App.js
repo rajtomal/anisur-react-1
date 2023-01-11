f8b4d49be74b1445bae4485744fc524f62c0321b
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2023-01-11 17:03:10 +0600

    Anisur Islam React First Project Practice -01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/card"
+import Data from "./data.json"
+
+
 
 function App() {
+
+  // let item = [];
+
+  // item = Data.map((one) => <Card titleName = {one.titleName} title = {one.title} detiles = {one.detiles}/>)
+
+  // for(let x=0; x<Data.length; x++){
+  //   item.push(<Card titleName = {Data[x].titleName} title = {Data[x].title} detiles = {Data[x].detiles}/>)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Todo List</h1>
+      {Data.map((item, index) => <Card key = {index} titleName = {item.titleName} title = {item.title} detiles = {item.detiles}/>)}
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,23 @@
+
+
+const date = new Date();
+const dateDay = date.getDate() ;
+const dateMonth = date.getMonth();
+const currentMonth = dateMonth + 1;
+const dateYear = date.getFullYear() ;
+
+function Card(props) {
+    const {titleName, title, detiles} = props; 
+    return(
+        <div className="card-parent">
+            <h1 >{titleName}</h1>
+            <h3 className="title">{title}</h3>
+            <p className="p-tag">{detiles}</p>
+            <p>{dateDay + "/" + currentMonth + "/" + dateYear}</p>
+        </div>
+    )
+}
+
+
+export default Card;
+
